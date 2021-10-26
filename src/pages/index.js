b154f0053b038628a5a2d1a3bef1c7e76a01399f
Francisco Cabral <franciscocabralfilho@gmail.com>
2021-10-26 14:46:57 -0300

    ituran icons

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,5 +2,11 @@ import React from "react"
 import styles from "./index.css"
 
 export default function Home() {
-  return <div className="banner"></div>
+  return <div className="banner">
+    <p class="banner-text">Logo mais com uma cara nova!
+      <br />
+      <br />
+      Entre em contato, <a alt='paulo.henrique@ituran.com.br' target="_blank" href='mailto:paulo.henrique@ituran.com.br?subject=Landing Page Contact'>paulo.henrique@ituran.com.br</a>
+    </p>
+  </div>
 }
