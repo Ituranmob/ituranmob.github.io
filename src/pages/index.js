59e7ac36a6b4203af65b1738f0704917117185ea
Francisco Cabral <franciscocabralfilho@gmail.com>
2021-11-10 17:19:56 -0300

    update js

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,13 +2,24 @@ import React from "react"
 import styles from "./index.css"
 
 export default function Home() {
-  return <div className="banner">
-    <p class="banner-text">Logo mais com uma cara nova!
-      <br />
-      <br />
-      Entre em contato,
-      <br />
-      <a alt='paulo.henrique@ituran.com.br' target="_blank" href='mailto:marketing@ituran.com.br?subject=Landing Page Contact'>marketing@ituran.com.br</a>
-    </p>
-  </div>
+  return <>
+    <div className="banner">
+      <p class="banner-text">Logo mais com uma cara nova!
+        <br />
+        <br />
+        Entre em contato,
+        <br />
+        <a alt='paulo.henrique@ituran.com.br' target="_blank" href='mailto:marketing@ituran.com.br?subject=Landing Page Contact'>marketing@ituran.com.br</a>
+      </p>
+    </div>
+    <div class="video">
+      <iframe width="100%" height="100%"
+        src="https://www.youtube-nocookie.com/embed/DVS3cxeplIw?controls=0"
+        title="YouTube video player"
+        frameborder="0"
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+        allowfullscreen>
+      </iframe>
+    </div>
+  </>
 }
